cdf14a728d725a509080f1bc79f5ea7d1f4ec119
Ralph Miole <blackspade2001@gmail.com>
2024-11-14 23:13:05 +0800

    logs and database added but cannot delete log

--- components/scripts.php
+++ components/scripts.php
@@ -1,3 +1,8 @@
+    <!-- SweetAlert2 Script -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="../js/scripts.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
